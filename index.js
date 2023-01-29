35b22e343be30b9a8f5fdb3def52408ac6cd83bc
Panuwat <tight.pw@gmail.com>
2023-01-29 20:46:29 +0700

    test api

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 4000;
-app.listen(PORT, () => {
-  console.log("API Listening on PORT ${PORT}");
-});
+const mysql = require("mysql2");
+const cors = require("cors");
+app.use(cors());
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
 app.get("/", (req, res) => {
   res.send("This is my API running");
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is my About Route");
+app.get("/att", (req, res) => {
+  connection.query(
+    "SELECT * FROM attractions",
+    function (err, resules, fields) {
+      res.send(resules);
+    }
+  );
 });
 
+app.listen(process.env.PORT || 4000);
+
 module.exports = app;
+//node index.js
